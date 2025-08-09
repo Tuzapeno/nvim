0f26d37f3b93fe377d8ea20f186584e8d9fbf256
Arthur Salerno <70231798+Tuzapeno@users.noreply.github.com>
2025-08-09 15:46:58 -0300

    fix(bufferline): Fix options configuration

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -12,24 +12,24 @@ vim.keymap.set('n', '<leader>bp', '<Cmd>BufferLinePick<CR>', { desc = "Pick buff
 vim.keymap.set('n', '<leader>bm', '<Cmd>BufferLineMoveNext<CR>', { desc = "Move buffer next" })
 vim.keymap.set('n', '<leader>bM', '<Cmd>BufferLineMovePrev<CR>', { desc = "Move buffer prev" })
 
--- Close all buffers except the current one
-vim.keymap.set('n', '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', { desc = "Close other buffers" })
 
 return {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-        require("bufferline").setup()
-    end,
-    opts = {
-        offsets = {
-            {
-                filetype = "NvimTree",
-                text = "File Explorer",
-                text_align = "center", -- | "center" | "right"
-                separator = true
+        require("bufferline").setup {
+            options = {
+                diagnostics = "nvim_lsp",
+                offsets = {
+                    {
+                        filetype = "NvimTree",
+                        text = "Workspace",
+                        text_align = "left",
+                        separator = true
+                    }
+                },
             }
-        },
-    }
+        }
+    end,
 }
